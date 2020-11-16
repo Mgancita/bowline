b2c42b4d95b2d8d7890961db54859c3421ecb2a5
Marco Gancitano <marco.gancitano97@gmail.com>
2020-11-15 20:28:16 -0500

    Auto generate version tag in publishing

--- bowline/__init__.py
+++ bowline/__init__.py
@@ -1,7 +1,12 @@
 """bowline namespace."""
 
+from importlib_metadata import version
+
 from .preprocessors import StandardPreprocessor
 
-__version__ = "0.1.0"
+try:
+    __version__ = version(__package__)
+except:
+    __version__ = "0.0.0"
 
 __all__ = ["StandardPreprocessor", "__version__"]
