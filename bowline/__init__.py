0dfe4213c8c6710f7d6f96c15700d5b3aed74c1f
Marco Gancitano <marco.gancitano97@gmail.com>
2020-11-25 10:04:46 -0500

    Add error catching to auto-version

--- bowline/__init__.py
+++ bowline/__init__.py
@@ -1,9 +1,12 @@
 """bowline namespace."""
 
-from importlib_metadata import version
+from importlib_metadata import PackageNotFoundError, version
 
 from .preprocessors import StandardPreprocessor
 
-__version__ = version(__package__)
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 __all__ = ["StandardPreprocessor", "__version__"]
