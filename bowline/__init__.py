4751fc9a425c2ec22d52b1aa2bd57634a5593c94
Marco Gancitano <marco.gancitano97@gmail.com>
2020-11-25 09:57:11 -0500

    Add pylint check for bowline directory

--- bowline/__init__.py
+++ bowline/__init__.py
@@ -4,9 +4,6 @@ from importlib_metadata import version
 
 from .preprocessors import StandardPreprocessor
 
-try:
-    __version__ = version(__package__)
-except:  # noqa: E722
-    __version__ = "0.0.0"
+__version__ = version(__package__)
 
 __all__ = ["StandardPreprocessor", "__version__"]
